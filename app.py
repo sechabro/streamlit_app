84af5db6abd7df085174bc6e93ba3d25d3e961a6
sechabro <96841300+sechabro@users.noreply.github.com>
2024-02-19 14:57:10 -0500

    app description in sidebar

--- app.py
+++ app.py
@@ -10,6 +10,14 @@ sorted_data = data_sorter.sort_data(data)
 st.set_page_config(
     page_title="Binance BTCUSDT Real-Time Trade Volume Data", layout="wide")
 
+with st.sidebar:
+    st.subheader(body="About this Websocket Application")
+    st.text_area(height=550, label='label', value='This websocket demo application displays live trading data from Binance, via the Finnhub API.'
+                 ' A call is made to the API every 2 seconds, the data is processed, and presented on this moving bar chart.'
+                 ' The y-axis represents BTC trading volume in BTC, and the x-axis represents the time at which trading action occurred.'
+                 ' The timestamps are in ascending order from right to left.'
+                 ' Data collection occurs for 3 minutes, and ceases upon reaching the time limit.', label_visibility="collapsed")
+
 if sorted_data is not None:
     st.header('Binance: BTCUSDT Trade Volume', divider=True)
     price = data_sorter.latest_price(sorted_data)
@@ -18,9 +26,7 @@ if sorted_data is not None:
 
     bar = st.bar_chart(data=sorted_data, x='Time',
                        y=['Buy Volume', 'Sell Volume'], color=["#79ea86", "#e75757"], height=550, width=1200, use_container_width=True)
-    with st.sidebar:
-        st.subheader(body="Additional Data")
-        st.text(body="This is an auto-running websocket application displays live trading data from Binance, via the Finnhub API. Data collection will cease when the time limit is reached.")
+
     time.sleep(2)
     st.rerun()
 
